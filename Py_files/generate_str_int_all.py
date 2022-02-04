d882ad7f80620a58f358a407344ca2c7aaa24b7d
Stanislav <69276462+stasgoodman@users.noreply.github.com>
2022-02-04 13:54:51 +0200

    Update generate_str_int_all.py
    31 Random letters: >>> FffDnoAhkSTmbduiOtnFdiRNxUwFZTL
    18 Random digits : >>> 447324461738687389
    20 Random all    : >>> ZG?R"WmRxf_aQMz]8e\g

--- Py_files/generate_str_int_all.py
+++ Py_files/generate_str_int_all.py
@@ -5,19 +5,19 @@ import string
 def gen_rand_str(length):
     letters = string.ascii_letters
     rand_str = ''.join(random.choice(letters) for _ in range(length))
-    print(length, "R Letters: >>>", rand_str)
+    print(length, "Random letters: >>>", rand_str)
 
 
 def gen_rand_num(r):
     numbers = string.digits
     rand_num = ''.join(random.choice(numbers)for _ in range(r))
-    print(r, "R Digits : >>>", rand_num)
+    print(r, "Random digits : >>>", rand_num)
 
 
 def get_rand_all(r):
     characters = string.ascii_letters + string.digits + string.punctuation
     rand_all = ''.join(random.choice(characters) for _ in range(r))
-    print(r, "R All    : >>>", rand_all)
+    print(r, "Random all    : >>>", rand_all)
 
 
 gen_rand_str(31)
